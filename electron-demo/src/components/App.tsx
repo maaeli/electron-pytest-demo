2bbaf3a2622cacf102abc409b03c9f3d13005a41
Martha <martha.brennich@googlemail.com>
2021-08-22 07:39:00 +0200

    Added button,

--- electron-demo/src/components/App.tsx
+++ electron-demo/src/components/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = (): JSX.Element => {
+    const [click, setClick] = useState(false);
     return (
-        <div>
-            <h1>💖 Hello World!</h1>
-            <p>Welcome to your Electron application.</p>
+        <div>{click
+            ? <div>
+                <h1>💖 Hello World!</h1>
+                <p>Welcome to your Electron application.</p>
+            </div>
+            : <button onClick={() => setClick(true)}>
+                Click me
+            </button>
+        }
         </div>
+
     );
 };
 
